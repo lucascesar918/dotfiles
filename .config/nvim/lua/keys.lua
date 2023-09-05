8dbc44f881b0be61bf87e88887e5d894efd68408
César <lucascesarfreitas@gmail.com>
2023-09-05 15:49:10 -0300

    Adding bind to ToggleTerm for Neovim

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -15,3 +15,14 @@ map('v', '<C- >', [[:'<,'>CommentToggle]] .. '<CR>', {})
 
 -- Save file
 map('n', '<C-S>', [[:w]] .. '<CR>', {})
+
+-- Toggle Terminal
+map('n', '<C-A>', [[:ToggleTerm dir=$PWD]] .. '<CR>', {})
+map('t', '<C-A>', [[:ToggleTerm]] .. '<CR>', {})
+map('t', '<esc>', [[<C-\><C-n>]], {})
+-- map('t', 'jk', [[<C-\><C-n>]], {})
+-- map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], {})
+-- map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], {})
+-- map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], {})
+-- map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], {})
+-- map('t', '<C-w>', [[<C-\><C-n><C-w>]], {})
